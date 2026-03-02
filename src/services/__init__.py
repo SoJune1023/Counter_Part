c3a2140fa73eb2af9375d996104ac8b864f78d5e
SoJune1023 <ssojune@naver.com>
2026-03-02 20:26:25 +0900

    feat: Add analyze_account_table

---
+++ src/services/__init__.py
@@ -0,0 +1,5 @@
+from src.services.analyze_account_table import analyze_account_table
+
+__all__ = [
+    analyze_account_table
+]
---
+++ src/services/analyze_account_table.py
@@ -0,0 +1,46 @@
+import polars as pl
+
+from typing import IO
+
+def analyze_account_table(
+    file_ptr: IO[bytes],
+    account_name: str,
+    *,
+    statement_num_col: str = "전표번호",
+    account_col: str = "계정과목",
+    chabun_col: str = "차변",
+    daebun_col: str = "대변",
+    summary_col: str = "적요"
+) -> pl.DataFrame:
+    df = pl.read_excel(file_ptr, engine="calamine")
+    lf = df.lazy().with_columns(
+        pl.col(statement_num_col).cast(pl.Utf8)
+    )
+
+    statement_numbers = ( # Get statement numbers as series WHERE account_col is account_name.
+        lf.filter(pl.col(account_col) == account_name)
+        .select(statement_num_col)
+        .unique()
+        .collect()
+        .to_series()
+    )
+
+    result = ( # Get data that we need.
+        lf.filter(pl.col(statement_num_col).is_in(statement_numbers))
+        .select([
+            statement_num_col,
+            account_col,
+            chabun_col,
+            daebun_col,
+            summary_col
+        ])
+        .sort(statement_num_col)
+        .collect()
+    )
+    """Expected result:
+    | 전표번호   | 계정과목 | 차변    | 대변    | 적요
+    | 2026-001 | 소모품비 | 50,000 | 0      | ...
+    | 2026-001 | 보통예금 | 0      | 50,000 | ...
+    """
+
+    return result
