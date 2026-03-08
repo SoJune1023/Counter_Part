17859a151979f0d6eaafb8a01319d73ee38e94e1
SoJune1023 <ssojune@naver.com>
2026-03-08 22:27:05 +0900

    feat: Add get_tb_table and remove simplification excel

--- src/handlers/__init__.py
+++ src/handlers/__init__.py
@@ -1,5 +1,5 @@
-from src.handlers.analyze import analyze_account_table, analyze_file, analyze_file_download, analyze_account_table_download
+from src.handlers.analyze import analyze_account_table, analyze_file, analyze_file_download, analyze_account_table_download, analyze_get_tb_table, analyze_get_tb_table_download
 
 __all__ = [
-    analyze_account_table, analyze_file, analyze_account_table_download, analyze_file_download
+    analyze_account_table, analyze_file, analyze_account_table_download, analyze_file_download, analyze_get_tb_table, analyze_get_tb_table_download
 ]
--- src/handlers/analyze.py
+++ src/handlers/analyze.py
@@ -7,7 +7,7 @@ logger = logging.getLogger(__name__)
 from fastapi import UploadFile
 from fastapi.responses import StreamingResponse, Response
 
-from src.services import AnalyzeAccountTable, AnalyzeGetExcel
+from src.services import AnalyzeAccountTable, AnalyzeGetExcel, AnalyzeGetTbTable
 from src.exceptions import InternalServerError, ClientError
 
 async def analyze_file(file: UploadFile):
@@ -118,6 +118,77 @@ async def analyze_account_table(file: UploadFile, account_name: str):
 async def analyze_account_table_download(file: UploadFile, account_name: str):
     try:
         analyzer = AnalyzeAccountTable()
+
+        content = await file.read()
+
+        loop = asyncio.get_running_loop()
+
+        await file.seek(0)
+
+        result = await loop.run_in_executor(
+            None,
+            analyzer.process,
+            content,
+            account_name
+        )
+        
+        # upload result file to memory
+        output = io.BytesIO()
+        result.write_excel(output)
+        output.seek(0)
+
+        # set file name
+        filename = f"result_{file.filename}"
+
+        # return result
+        return StreamingResponse(
+            output,
+            media_type="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+            headers={"Content-Disposition": f"attachment; filename={filename}"}
+        )
+    except (ClientError, InternalServerError):
+        raise
+    except Exception as e:
+        logger.error("Unexpected exception detected while analyze data. Please contact me. email: ssojune@naver.com.", exc_info=True)
+        raise InternalServerError(f"Unexpected exception on /analyze/account_table POST. Exc info: {str(e)}", 500) from e
+
+async def analyze_get_tb_table(file: UploadFile):
+    try:
+        loader = AnalyzeGetTbTable()
+
+        content = await file.read()
+
+        loop = asyncio.get_running_loop()
+
+        await file.seek(0)
+
+        result = await loop.run_in_executor(
+            None,
+            loader.process,
+            content
+        )
+
+        arrow_table = result.to_arrow()
+
+        sink = io.BytesIO()
+        with pa.ipc.new_stream(sink, arrow_table.schema) as writer:
+            writer.write_table(arrow_table)
+        
+        await file.seek(0)
+        
+        return Response(
+            content=sink.getvalue(),
+            media_type="application/vnd.apache.arrow.stream" # 스트림 전용 MIME 타입
+        )
+    except (ClientError, InternalServerError):
+        raise
+    except Exception as e:
+        logger.error("Unexpected exception detected while get excel data. Please contact me. email: ssojune@naver.com.", exc_info=True)
+        raise InternalServerError(f"Unexpected exception on /analyze GET. Exc info: {str(e)}", 500) from e
+
+async def analyze_get_tb_table_download(file: UploadFile, account_name: str):
+    try:
+        analyzer = AnalyzeGetTbTable()
 
         content = await file.read()
 
