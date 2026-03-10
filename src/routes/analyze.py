df60d0fd6e647e528a43676e384e89e4aa76fd9c
SoJune1023 <ssojune@naver.com>
2026-03-10 17:42:17 +0900

    refactor: Refactor to edit column name easy

--- src/routes/analyze.py
+++ src/routes/analyze.py
@@ -4,7 +4,9 @@ import sys
 from fastapi import APIRouter, status, File, UploadFile, Form, Request
 from fastapi.templating import Jinja2Templates
 from fastapi.responses import HTMLResponse
+from typing import Annotated
 
+from src.schemas import AnalyzeRequestSchema
 from src.handlers import (
     analyze_account_table, analyze_account_table_download,
     analyze_get_tb_table, analyze_get_tb_table_download
@@ -29,31 +31,93 @@ async def get_main_page(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
 
 @route.post("/file", tags=['file'], status_code=status.HTTP_200_OK)
-async def post_nalyze_get_tb_table(
-    file: UploadFile = File(...)
+async def post_analyze_get_tb_table(
+    file: Annotated[UploadFile, File()],
+    statement_id_col: Annotated[str, File()],
+    account_id_col: Annotated[str, File()],
+    debit_col: Annotated[str, File()],
+    credit_col: Annotated[str, File()],
+    summary_col: Annotated[str, File()]
 ):
     """Get data for main page"""
-    return await analyze_get_tb_table(file)
+
+    data = AnalyzeRequestSchema(
+        statement_id_col=statement_id_col,
+        account_id_col=account_id_col,
+        debit_col=debit_col,
+        credit_col=credit_col,
+        summary_col=summary_col
+    )
+
+    return await analyze_get_tb_table(file, data)
 
 @route.post("/file/download", tags=['download'], status_code=status.HTTP_200_OK)
 async def post_nalyze_get_tb_table_download(
-    file: UploadFile = File(...)
+    file: Annotated[UploadFile, File()],
+    statement_id_col: Annotated[str, File()],
+    account_id_col: Annotated[str, File()],
+    debit_col: Annotated[str, File()],
+    credit_col: Annotated[str, File()],
+    summary_col: Annotated[str, File()]
 ):
     """Download simplification file."""
-    return await analyze_get_tb_table_download(file)
+
+    data = AnalyzeRequestSchema(
+        statement_id_col=statement_id_col,
+        account_id_col=account_id_col,
+        debit_col=debit_col,
+        credit_col=credit_col,
+        summary_col=summary_col
+    )
+
+    return await analyze_get_tb_table_download(file, data)
 
 @route.post("/account_table", tags=['account_table'], status_code=status.HTTP_200_OK)
 async def post_analyze_account_table(
-    file: UploadFile = File(...),
-    account_name: str = Form(...)
+    file: Annotated[UploadFile, File()],
+    selected_account_id: Annotated[str, File()],
+    classification_col: Annotated[str, File()],
+    statement_id_col: Annotated[str, File()],
+    account_id_col: Annotated[str, File()],
+    debit_col: Annotated[str, File()],
+    credit_col: Annotated[str, File()],
+    summary_col: Annotated[str, File()]
 ):
     """Get excel file and return analyze result."""
-    return await analyze_account_table(file, account_name)
+
+    data = AnalyzeRequestSchema(
+        selected_account_id=selected_account_id,
+        classification_col=classification_col,
+        statement_id_col=statement_id_col,
+        account_id_col=account_id_col,
+        debit_col=debit_col,
+        credit_col=credit_col,
+        summary_col=summary_col
+    )
+
+    return await analyze_account_table(file, data)
 
 @route.post("/account_table/download", tags=['download'], status_code=status.HTTP_200_OK)
 async def post_analyze_account_table_download(
-    file: UploadFile = File(...),
-    account_name: str = Form(...)
+    file: Annotated[UploadFile, File()],
+    selected_account_id: Annotated[str, File()],
+    classification_col: Annotated[str, File()],
+    statement_id_col: Annotated[str, File()],
+    account_id_col: Annotated[str, File()],
+    debit_col: Annotated[str, File()],
+    credit_col: Annotated[str, File()],
+    summary_col: Annotated[str, File()]
 ):
     """Get excel file and return analyze file."""
-    return await analyze_account_table_download(file, account_name)
+
+    data = AnalyzeRequestSchema(
+        selected_account_id=selected_account_id,
+        classification_col=classification_col,
+        statement_id_col=statement_id_col,
+        account_id_col=account_id_col,
+        debit_col=debit_col,
+        credit_col=credit_col,
+        summary_col=summary_col
+    )
+
+    return await analyze_account_table_download(file, data)
